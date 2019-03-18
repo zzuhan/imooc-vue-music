9d63b2243648c5a2e00a26b12988d931657fa405
sea-breeze <1595575913@qq.com>
2019-03-18 22:29:19 +0800

    说明

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import 'babel-polyfill'; // 将整个库都导入进来：将该库导出的变量直接添加到本模块的命名空间中。
+import 'babel-polyfill'; // 将整个库都导入进来：将该库导出的变量直接添加到本模块的命名空间中。三种导入方式之一。
 import Vue from 'vue';
 import App from './app';
 import fastclick from 'fastclick';
