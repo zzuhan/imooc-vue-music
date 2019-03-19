5dd3954352bc9a0e066fb6464edd70ee36021ee3
sea-breeze <1595575913@qq.com>
2019-03-19 12:08:06 +0800

    说明

--- build/utils.js
+++ build/utils.js
@@ -22,6 +22,8 @@ exports.cssLoaders = function (options) {
     }
   }
 
+  // PostCSS支持 CSS Variables 和 Mixins， 编译尚未被浏览器广泛支持的先进的 CSS 语法，内联图片，以及其它很多优秀的功能。
+  // PostCSS 的 Autoprefixer 插件是最流行的 CSS 处理工具之一。可以自动添加浏览器前缀。
   var postcssLoader = {
     loader: 'postcss-loader',
     options: {
@@ -30,6 +32,7 @@ exports.cssLoaders = function (options) {
   }
 
   // generate loader string to be used with extract text plugin
+  // 从右向左加载：右边的加载器先加载。例如，[loader1,loader2,loader3]的加载顺序是3->2->1
   function generateLoaders(loader, loaderOptions) {
     const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
     if (loader) {
@@ -43,12 +46,15 @@ exports.cssLoaders = function (options) {
 
     // Extract CSS when that option is specified
     // (which is the case during production build)
+    // 执行npm run build时，打包生产环境，将样式提取到单独的css文件中
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader' // 未被提取为单独文件的css块，使用哪个加载器加载
       })
-    } else {
+    } 
+    // 执行npm run dev时，打包非生产环境，将样式作为内联样式添加在index.html中
+    else {
       return ['vue-style-loader'].concat(loaders)
     }
   }
--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -13,6 +13,8 @@ module.exports = {
     extract: isProduction
   }),
   cssSourceMap: sourceMapEnabled,
+  // transformAssetUrls选项
+  // 在模板编译过程中，编译器可以将某些特性转换为 require 调用，例如 src 中的 URL。因此这些目标资源可以被 webpack 处理。
   transformToRequire: {
     video: 'src',
     source: 'src',
