31e5e67eca2799af8e384a27dba6aa888017871c
sea-breeze <1595575913@qq.com>
2019-03-20 18:06:06 +0800

    歌手列表

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="singer" ref="singer">
+        <list-view :data="singers" ref="list" />
+    </div>
+</template>
+
+<script>
+import { getSingerList } from "api/singer";
+import { ERR_OK } from "api/config";
+import ListView from "base/list-view/list-view";
+import Singer from "common/js/singer";
+
+const HOT_SINGER_LEN = 10;
+const HOT_NAME = "热门";
+
+export default {
+  data() {
+    return {
+      singers: [] // 分类数组：每个成员是一个分类。每个分类的items属性又是一个数组（该分类下的所有歌手）
+    };
+  },
+  created() {
+    this._getSingerList();
+  },
+  methods: {
+    //  获取歌手列表
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list);
+        }
+      });
+    },
+    // 聚合歌手列表
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      };
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(
+            new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            })
+          );
+        }
+        const key = item.Findex;
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          };
+        }
+        map[key].items.push(
+          new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          })
+        );
+      });
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []; // A~Z
+      let hot = []; // 热门
+      for (let key in map) {
+        let val = map[key];
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val);
+        } else if (val.title === HOT_NAME) {
+          hot.push(val);
+        }
+      }
+      // 字母排序
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+      });
+      return hot.concat(ret);
+    }
+  },
+  components: { ListView }
+};
+</script>
+
+<style lang="stylus" scoped>
+.singer {
+    position: fixed;
+    top: 88px;
+    bottom: 0;
+    width: 100%;
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Singer from "components/singer/singer";
 
-const Recommend = ()=>import("components/recommend/recommend");
+const Recommend = () => import("components/recommend/recommend");
 
 // 安装 Vue.js 插件。
 // 该方法需要在调用 new Vue() 之前被调用。
@@ -16,6 +17,10 @@ export default new Router({
     {
       path: "/recommend",
       component: Recommend
+    },
+    {
+      path: "/singer",
+      component: Singer
     }
   ]
 });
